d56547b886feb3d4eddbef118417ed5539ce8ba0
davakanbendicto@gmail.com <davakanbendicto@gmail.com>
2022-09-06 10:55:42 +0100

    Ajout des groupes et des permissions depuis le dashboard du superuser

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,7 +1,9 @@
 from pyexpat import model
 from attr import fields
 from django import forms
-
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 from accounts.models import CustomUser, Profile
 from hotels.models import Chambre, Equipement, Equipement_Hotel, Hotel, Image_Chambre, Image_Hotel, Payement
 
@@ -17,6 +19,43 @@ class SignupForm(forms.Form):
 class SigninForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(min_length=6, widget=forms.PasswordInput())
+    
+
+class UserCreationForm(UserCreationForm):
+
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(UserChangeForm):
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password', 'first_name', 'last_name',
+                  'is_active', 'is_admin','groups', 'user_permissions',)
 
 
 class AddHotelImg(forms.ModelForm):
