d3bc767b76e03a1bd7d1b151100daaa69a09606c
davakanbendicto@gmail.com <davakanbendicto@gmail.com>
2022-12-12 10:17:41 +0100

    12/12/22

--- src/hotels/templatetags/site_public/tags_hotel.py
+++ src/hotels/templatetags/site_public/tags_hotel.py
@@ -2,7 +2,7 @@ from django import template
 from django.shortcuts import get_object_or_404
 from hotels.models import Category, Hotel, Chambre
 
-from accounts.models import HotelRatings
+from accounts.models import HotelRatings, RoomRatings
 
 register = template.Library()
 
@@ -40,6 +40,20 @@ def get_hotel_score(token):
 
 
 @register.filter
+def get_room_score(id):
+
+    room = get_object_or_404(Chambre, pk=id)
+
+    room_ratings = RoomRatings.objects.filter(room=room.id)
+
+    score = 0
+    for rating in room_ratings:
+        score += rating.score
+
+    return score / len(room_ratings) if score else ''
+
+
+@register.filter
 def get_chambre_by_hotel(item):
     category = Category.objects.get(id=item.id)
     chambres = Chambre.objects.filter(category_id=category.id)
@@ -52,5 +66,15 @@ def get_hotel_experiences(token):
     hotel = get_object_or_404(Hotel, token=token)
 
     expreriences = HotelRatings.objects.filter(hotel=hotel.id)
+
+    return expreriences.count()
+
+
+@register.filter
+def get_room_experiences(id):
+
+    room = get_object_or_404(Chambre, pk=id)
+
+    expreriences = RoomRatings.objects.filter(room=room.id)
 
     return expreriences.count()
